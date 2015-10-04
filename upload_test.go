c5f52c409357efa4322eb620a8643f37abea52ad
Max Kueng <me@maxkueng.com>
2015-10-05 00:17:51 +0200

    skip tests with authentication if environment variables are not present

--- upload_test.go
+++ upload_test.go
@@ -1,6 +1,7 @@
 package streamable
 
 import (
+	"os"
 	"path"
 	"testing"
 
@@ -10,20 +11,32 @@ import (
 var testFilesDir = path.Join(".", "test-files")
 
 func Test_Upload(t *testing.T) {
-	var testFile = path.Join(testFilesDir, "cat-video.mp4")
+	testFile := path.Join(testFilesDir, "cat-video.mp4")
 
 	res, err := Upload(testFile)
 	assert.Nil(t, err)
 	assert.NotZero(t, res)
 }
 
+func Test_Upload_NonExistentFile(t *testing.T) {
+	testFile := path.Join(testFilesDir, "not-exists.mp4")
+
+	res, err := Upload(testFile)
+	assert.NotNil(t, err)
+	assert.Zero(t, res)
+}
+
 func Test_UploadAuthenticated_UsernamePassword(t *testing.T) {
-	var testFile = path.Join(testFilesDir, "cat-video.mp4")
+	if os.Getenv("STREAMABLE_USERNAME") == "" || os.Getenv("STREAMABLE_PASSWORD") == "" {
+		t.Skip("skipping test; $STREAMABLE_USERNAME or $STREAMABLE_PASSWORD not set")
+	}
 
 	creds := Credentials{
-		Username: "gostreamabletest",
-		Password: "gostreamabletest0=",
+		Username: os.Getenv("STREAMABLE_USERNAME"),
+		Password: os.Getenv("STREAMABLE_PASSWORD"),
 	}
+
+	testFile := path.Join(testFilesDir, "cat-video.mp4")
 
 	res, err := UploadAuthenticated(creds, testFile)
 	assert.Nil(t, err)
