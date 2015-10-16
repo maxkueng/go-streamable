58021989327f03075fc6be110687f165959bd32b
Max Kueng <me@maxkueng.com>
2015-10-17 00:59:10 +0200

    move functions in to a single file

--- streamable.go
+++ streamable.go
@@ -1,3 +1,144 @@
 package streamable
 
-const apiUrl string = "https://api.streamable.com"
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+const (
+	apiUrl    string = "https://api.streamable.com"
+	importUrl string = apiUrl + "/import"
+	uploadUrl string = apiUrl + "/upload"
+)
+
+func UploadVideo(filePath string) (VideoResponse, error) {
+	return uploadVideo(Credentials{}, filePath)
+}
+
+func UploadVideoAuthenticated(creds Credentials, filePath string) (VideoResponse, error) {
+	return uploadVideo(creds, filePath)
+}
+
+func uploadVideo(creds Credentials, filePath string) (VideoResponse, error) {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		return VideoResponse{}, err
+	}
+
+	var buf bytes.Buffer
+
+	multipartWriter := multipart.NewWriter(&buf)
+
+	fileHandle, err := os.Open(filePath)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	fileWriter, err := multipartWriter.CreateFormFile("file", filePath)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	_, err = io.Copy(fileWriter, fileHandle)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	multipartWriter.Close()
+
+	req, err := http.NewRequest("POST", uploadUrl, &buf)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	if creds.Username != "" && creds.Password != "" {
+		req.SetBasicAuth(creds.Username, creds.Password)
+	}
+
+	req.Header.Set("Content-Type", multipartWriter.FormDataContentType())
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return VideoResponse{}, fmt.Errorf("upload failed")
+	}
+
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	body := bytesToString(bodyBytes)
+
+	videoRes, err := videoResponseFromJson(body)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	return videoRes, nil
+}
+
+func ImportVideoFromUrl(videoUrl string) (VideoResponse, error) {
+	return importVideoFromUrl(Credentials{}, videoUrl)
+}
+
+func ImportVideoFromUrlAuthenticated(creds Credentials, videoUrl string) (VideoResponse, error) {
+	return importVideoFromUrl(creds, videoUrl)
+}
+
+func importVideoFromUrl(creds Credentials, videoUrl string) (VideoResponse, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", getImportUrl(videoUrl), nil)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	if creds.Username != "" && creds.Password != "" {
+		req.SetBasicAuth(creds.Username, creds.Password)
+	}
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return VideoResponse{}, fmt.Errorf("not found")
+	}
+
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	body := bytesToString(bodyBytes)
+
+	videoRes, err := videoResponseFromJson(body)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	return videoRes, nil
+
+}
+
+func getImportUrl(videoUrl string) string {
+	parsedUrl, _ := url.Parse(importUrl)
+	q := parsedUrl.Query()
+	q.Set("url", videoUrl)
+	parsedUrl.RawQuery = q.Encode()
+
+	return parsedUrl.String()
+}
--- streamable_test.go
+++ streamable_test.go
@@ -1,6 +1,7 @@
 package streamable
 
 import (
+	"fmt"
 	"os"
 	"path"
 	"testing"
@@ -41,4 +42,32 @@ func Test_UploadVideoAuthenticated_UsernamePassword(t *testing.T) {
 	res, err := UploadVideoAuthenticated(creds, testFile)
 	assert.Nil(t, err)
 	assert.NotZero(t, res)
+}
+
+func Test_ImportVideo(t *testing.T) {
+	videoUrl := "https://archive.org/download/Windows7WildlifeSampleVideo/Wildlife.wmv"
+	res, err := ImportVideoFromUrl(videoUrl)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 1, res.Status)
+	assert.NotEqual(t, "", res.Shortcode)
+}
+
+func Test_ImportVideoFromUrlAuthenticated_UsernamePassword(t *testing.T) {
+	if os.Getenv("STREAMABLE_USERNAME") == "" || os.Getenv("STREAMABLE_PASSWORD") == "" {
+		t.Skip("skipping test; $STREAMABLE_USERNAME or $STREAMABLE_PASSWORD not set")
+	}
+
+	creds := Credentials{
+		Username: os.Getenv("STREAMABLE_USERNAME"),
+		Password: os.Getenv("STREAMABLE_PASSWORD"),
+	}
+
+	videoUrl := "https://archive.org/download/Windows7WildlifeSampleVideo/Wildlife.wmv"
+	res, err := ImportVideoFromUrlAuthenticated(creds, videoUrl)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 1, res.Status)
+	assert.NotEqual(t, "", res.Shortcode)
+	fmt.Printf("%v\n", res)
 }
