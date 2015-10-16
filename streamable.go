0e859ee28ac8e72595e2638b2a786b4390387d31
Max Kueng <me@maxkueng.com>
2015-10-17 01:00:26 +0200

    move private functions down

--- streamable.go
+++ streamable.go
@@ -25,6 +25,61 @@ func UploadVideoAuthenticated(creds Credentials, filePath string) (VideoResponse
 	return uploadVideo(creds, filePath)
 }
 
+func ImportVideoFromUrl(videoUrl string) (VideoResponse, error) {
+	return importVideoFromUrl(Credentials{}, videoUrl)
+}
+
+func ImportVideoFromUrlAuthenticated(creds Credentials, videoUrl string) (VideoResponse, error) {
+	return importVideoFromUrl(creds, videoUrl)
+}
+
+func importVideoFromUrl(creds Credentials, videoUrl string) (VideoResponse, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", getImportUrl(videoUrl), nil)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	if creds.Username != "" && creds.Password != "" {
+		req.SetBasicAuth(creds.Username, creds.Password)
+	}
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return VideoResponse{}, fmt.Errorf("not found")
+	}
+
+	bodyBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	body := bytesToString(bodyBytes)
+
+	videoRes, err := videoResponseFromJson(body)
+	if err != nil {
+		return VideoResponse{}, err
+	}
+
+	return videoRes, nil
+
+}
+
+func getImportUrl(videoUrl string) string {
+	parsedUrl, _ := url.Parse(importUrl)
+	q := parsedUrl.Query()
+	q.Set("url", videoUrl)
+	parsedUrl.RawQuery = q.Encode()
+
+	return parsedUrl.String()
+}
+
 func uploadVideo(creds Credentials, filePath string) (VideoResponse, error) {
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		return VideoResponse{}, err
@@ -86,59 +141,4 @@ func uploadVideo(creds Credentials, filePath string) (VideoResponse, error) {
 	}
 
 	return videoRes, nil
-}
-
-func ImportVideoFromUrl(videoUrl string) (VideoResponse, error) {
-	return importVideoFromUrl(Credentials{}, videoUrl)
-}
-
-func ImportVideoFromUrlAuthenticated(creds Credentials, videoUrl string) (VideoResponse, error) {
-	return importVideoFromUrl(creds, videoUrl)
-}
-
-func importVideoFromUrl(creds Credentials, videoUrl string) (VideoResponse, error) {
-	client := &http.Client{}
-
-	req, err := http.NewRequest("GET", getImportUrl(videoUrl), nil)
-	if err != nil {
-		return VideoResponse{}, err
-	}
-
-	if creds.Username != "" && creds.Password != "" {
-		req.SetBasicAuth(creds.Username, creds.Password)
-	}
-
-	res, err := client.Do(req)
-	defer res.Body.Close()
-	if err != nil {
-		return VideoResponse{}, err
-	}
-
-	if res.StatusCode != http.StatusOK {
-		return VideoResponse{}, fmt.Errorf("not found")
-	}
-
-	bodyBytes, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return VideoResponse{}, err
-	}
-
-	body := bytesToString(bodyBytes)
-
-	videoRes, err := videoResponseFromJson(body)
-	if err != nil {
-		return VideoResponse{}, err
-	}
-
-	return videoRes, nil
-
-}
-
-func getImportUrl(videoUrl string) string {
-	parsedUrl, _ := url.Parse(importUrl)
-	q := parsedUrl.Query()
-	q.Set("url", videoUrl)
-	parsedUrl.RawQuery = q.Encode()
-
-	return parsedUrl.String()
 }
