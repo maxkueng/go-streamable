1921ab5415fbe61413943bb90003f242ff415302
Max Kueng <me@maxkueng.com>
2015-10-17 02:01:53 +0200

    typos

--- credentials.go
+++ credentials.go
@@ -1,6 +1,6 @@
 package streamable
 
-// Credentials holds user credentials used form athenticated requests.
+// Credentials holds user credentials used for authenticated requests.
 type Credentials struct {
 	Username string `json:username`
 	Password string `json:password`
