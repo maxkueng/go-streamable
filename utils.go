9a332ae05c9b5aef62dc9595fa5771af2e0b2ebd
Max Kueng <me@maxkueng.com>
2015-10-17 19:49:10 +0200

    add function to authenticate HTTP requests

--- utils.go
+++ utils.go
@@ -1,5 +1,13 @@
 package streamable
 
+import "net/http"
+
 func bytesToString(byteArray []byte) string {
 	return string(byteArray)
+}
+
+func authenticateHTTPRequest(req *http.Request, creds Credentials) {
+	if creds.Username != "" && creds.Password != "" {
+		req.SetBasicAuth(creds.Username, creds.Password)
+	}
 }
